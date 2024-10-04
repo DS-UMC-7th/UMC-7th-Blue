b575a75abe5c4c43ec5f62ba4d27fa59113f1946
fsdffds <qhals_01@naver.com>
2024-10-04 17:46:05 +0900

    2주차 미션1

---
+++ week2-mission1/src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ type, onClick, children, className }) => {
+  return (
+    <button type={type} onClick={onClick} className={className}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
